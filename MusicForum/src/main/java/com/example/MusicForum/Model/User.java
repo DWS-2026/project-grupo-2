0a4a920e46cc469e9aa3a3d3e411109a0360ad1d
msbuns01 <164387846+msbuns01@users.noreply.github.com>
2026-02-17 10:46:09 +0100

    Update User.java
    Creado "Model" de la entidad usuario

--- MusicForum/src/main/java/com/example/MusicForum/Model/User.java
+++ MusicForum/src/main/java/com/example/MusicForum/Model/User.java
@@ -1,5 +1,66 @@
 package com.example.MusicForum.Model;
 
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String avatar;
+    private String userRole; // "USER" "INVITADO" "ADMIN"
     
+    public User(){
+    }
+    
+    public User(String username, String password, String email, String userRole){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.userRole = userRole;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getAvatar(){
+        return avatar;
+    }
+
+    public void setAvatar(String avatar){
+        this.avatar = avatar;
+    }
+    
+    public String getUserRole(){
+        return userRole;
+    }
+
+    public void setUserRole(String userRole){
+        this.userRole = userRole;
+    }
+
+    //UserRole
+    public enum UserRole{
+        USER,
+        INVITADO,
+        ADMIN
+    }
 }
