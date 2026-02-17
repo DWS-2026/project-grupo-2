460d2b52e764253ab082709d99764633950fc36b
OscarRP15 <o.rodriguez.2022@alumnos.urjc.es>
2026-02-17 11:11:16 +0100

    added jpa dependency and updated comment and post

--- MusicForum/src/main/java/com/example/MusicForum/Model/Comment.java
+++ MusicForum/src/main/java/com/example/MusicForum/Model/Comment.java
@@ -1,21 +1,68 @@
 package com.example.MusicForum.Model;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "comments")
+public class Comment {
 
-
-public class Comment{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ID;
+
+    @Column(columnDefinition = "TEXT") // Permite comentarios largos
     private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // Nombre de la columna en la tabla de la DB
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id") // Relación con el Post
     private Post post;
 
-    public Comment(){}
+    // Constructor vacío (Obligatorio para JPA)
+    public Comment() {}
 
-    public Comment(Long ID,String comment,User user, Post post){
-        this.ID=ID;
-        this.comment=comment;
-        this.user=user;
-        this.post=post;
+    // Constructor con parámetros
+    public Comment(Long ID, String comment, User user, Post post) {
+        this.ID = ID;
+        this.comment = comment;
+        this.user = user;
+        this.post = post;
     }
 
+    // --- GETTERS Y SETTERS ---
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
--- MusicForum/src/main/java/com/example/MusicForum/Model/Post.java
+++ MusicForum/src/main/java/com/example/MusicForum/Model/Post.java
@@ -1,36 +1,63 @@
 package com.example.MusicForum.Model;
 
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ID;
+
     private String title;
-    private User user;
+    
+    @Column(columnDefinition = "TEXT")
     private String description;
+    
     private String image;
-    private List<Comments> comments=new ArrayList<>();
 
-    public Post(){}
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    // Un Post tiene muchos comentarios
+    // mappedBy debe coincidir con el nombre del atributo "post" en la clase Comment
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
-    public Post(Long ID,String title,String description,String image,User user){
-        this.ID=ID;
-        this.title=title;
-        this.description=description;
-        this.image=image;
-        this.user=user;
+    // Un Post tiene muchos álbumes (asumiendo que en la clase Albums hay un atributo 'post')
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Albums> albums = new ArrayList<>();
+
+    // Constructor vacío obligatorio para JPA
+    public Post() {}
+
+    // Constructor con parámetros
+    public Post(Long ID, String title, String description, String image, User user) {
+        this.ID = ID;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.user = user;
     }
+
+    // --- GETTERS Y SETTERS ---
 
     public Long getID() { 
         return ID; 
     }
-    public Long setID(Long ID) { 
+
+    public void setID(Long ID) { // Corregido: antes tenías Long de retorno en lugar de void
         this.ID = ID; 
     }
+
     public String getTitle() { 
         return title; 
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -57,5 +84,21 @@ public class Post {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Albums> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Albums> albums) {
+        this.albums = albums;
     }
 }
