e5e5c1d29f676299632f54e019e655c176f81e45
msbuns01 <164387846+msbuns01@users.noreply.github.com>
2026-02-20 12:15:26 +0100

    UserRepository.java

---
+++ MusicForum/src/main/java/com/example/MusicForum/Controller/UserController.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.MusicForum.Model.Comment;
+import com.example.MusicForum.Model.Post;
+import com.example.MusicForum.Repository.PostRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Controller
+public class UserController {
+    
+    
+}
