ab16d8fb533136b725b22f610a7715077357a8a2
roomtrash6 <c.morenoc.2022@alumnos.urjc.es>
2026-03-03 10:05:35 +0100

    some example data for the album database for testing

--- MusicForum/src/main/java/com/example/MusicForum/Service/DataBaseInit.java
+++ MusicForum/src/main/java/com/example/MusicForum/Service/DataBaseInit.java
@@ -1,9 +1,15 @@
 package com.example.MusicForum.Service;
 
+import com.example.MusicForum.Model.Album;
 import com.example.MusicForum.Model.Comment;
 import com.example.MusicForum.Model.Post;
+import com.example.MusicForum.Repository.AlbumRepository;
 import com.example.MusicForum.Repository.PostRepository;
 import jakarta.annotation.PostConstruct;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +18,17 @@ public class DataBaseInit {
 
     @Autowired
     private PostRepository postRepository;
-
+    private AlbumRepository albumRepository;
     @PostConstruct
     public void init() {
         // Inicialización de datos de prueba
         Post post1 = new Post("Post1", "2024-01-01", "Descripción del post 1", "/images/rosalia-lux.jpg", null);
         Post post2 = new Post("Post2", "2024-01-02", "Descripción del post 2", "/images/Geese-Getting-Killed.jpg", null);
         Post post3 = new Post("Post3", "2024-01-03", "Descripción del post 3", "image3.jpg", null);
+        List<String> songs = Arrays.asList("953", "Speedway", "Reggae", "Near DT, MI", "Western", "Of Schlagenheim", "bmbmbm", "Years Ago", "Ducter");
+
+        Album Album1= new Album("Schlagenheim", songs , "schlagenheim.png", "21-06-2019", "black midi");
+        albumRepository.save(Album1);
 
         post1.addComment(new Comment("Comentario 1"));
         post2.addComment(new Comment("Comentario 2"));
