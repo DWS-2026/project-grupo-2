fd9a99ecd5c46b5d8225ba194d37ab23ed7507ce
msbuns01 <164387846+msbuns01@users.noreply.github.com>
2026-05-07 20:03:31 +0200

    Test to add api-docs

--- MusicForum/src/main/java/com/example/MusicForum/Security/WebSecurityConfig.java
+++ MusicForum/src/main/java/com/example/MusicForum/Security/WebSecurityConfig.java
@@ -66,7 +66,8 @@ public SecurityFilterChain apiFilterChain(HttpSecurity http,
             //Public URLs
             .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll() 
             .requestMatchers(HttpMethod.GET, "/api/v1/albums/**").permitAll()
-            .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register").permitAll() // Login and REegister
+            .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register").permitAll() // Login and Register
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()  //26. 
             // Only ADMIN role can perform DELETE requests on /api/v1/posts/**
             .requestMatchers(HttpMethod.DELETE, "/api/v1/posts/**").hasRole("ADMIN")
             .requestMatchers(HttpMethod.DELETE, "/api/v1/albums/**").hasRole("ADMIN")
